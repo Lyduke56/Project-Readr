65a4dc5fdf68fa1d62836303cb86e085b543c143
AxzielB <axzieljaybartolabac@gmail.com>
2025-07-07 23:10:31 +0800

    [fixes][bartolabac minor capitalization fix

--- project-readr/src/Modal/forgotPass.jsx
+++ project-readr/src/Modal/forgotPass.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
-import './ForgotPass.css';
+import './forgotPass.css';
 
 export const ForgotPass = ({ email: initialEmail = '' }) => {
   const [email, setEmail] = useState(initialEmail);
